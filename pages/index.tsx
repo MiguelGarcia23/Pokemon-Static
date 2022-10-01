c373372ae32b0d5876b17faf7dbcc5eb5107bc13
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-01 12:11:00 -0400

    Implementando NextUI al proyecto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@nextui-org/react';
 import { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <Button color="gradient">
+        Hola Mundo
+      </Button>
     </>
   )
 }
