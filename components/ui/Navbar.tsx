6305ef39266c89c362a148b3d8d15ce23e1915af
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-03 17:03:48 -0400

    Haciendo funcional el Navbar y creando la página de favoritos

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { Spacer, Text, useTheme } from "@nextui-org/react";
+import NextLink from "next/link";
+import { Link, Spacer, Text, useTheme } from "@nextui-org/react";
 
 
 export const Navbar = () => {
@@ -13,7 +14,7 @@ export const Navbar = () => {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'start',
-        padding: '0px 20px',
+        padding: '0 20px',
         backgroundColor: theme?.colors.gray900.value
     }}>
         <Image 
@@ -23,12 +24,20 @@ export const Navbar = () => {
           height={70}
         />
 
-        <Text color="white" h2>P</Text>
-        <Text color="white" h3>okémon</Text>
+        <NextLink href='/' passHref>
+          <Link>
+            <Text color="white" h2>P</Text>
+            <Text color="white" h3>okémon</Text>
+          </Link>
+        </NextLink>
 
         <Spacer css={{ flex: 1 }} />
 
-        <Text color="white">Favoritos</Text>
+        <NextLink href='/favorites' passHref>
+          <Link>
+            <Text color="white">Favoritos</Text> 
+          </Link>
+        </NextLink>
     </div>
   )
 }
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../components/layouts";
+
+const FavoritesPage = () => {
+  return (
+    <Layout title="Pokémons - Favoritos">
+        <h1>Favoritos</h1>
+    </Layout>
+  )
+}
+
+export default FavoritesPage;
