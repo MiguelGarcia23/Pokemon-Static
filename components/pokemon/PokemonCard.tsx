36ac1579e586b11a5ba8f6b66d7496fd8b744b43
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-03 16:23:44 -0400

    Creando y haciendo funcional las rutas de las páginas de cada pokemon. Uso de GetStaticPaths

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,4 +1,6 @@
 import { FC } from "react";
+import { useRouter } from "next/router";
+
 import { Grid, Card, Row, Text } from '@nextui-org/react';
 
 import { SmallPokemon } from "../../interfaces";
@@ -8,9 +10,20 @@ interface Props {
 }
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
+
+    const router = useRouter();
+
+    const onClick = () => {
+        router.push(`/pokemon/${ pokemon.id }`)
+    }
+
     return (
         <Grid xs={ 6 } sm={ 3 } md={ 2 } xl={ 1 } key={ pokemon.id }>
-        <Card hoverable clickable>
+        <Card 
+            hoverable 
+            clickable
+            onClick={ onClick }
+        >
             <Card.Body css={{ p: 1 }}>
             <Card.Image 
                 src={ pokemon.img } 
---
+++ pages/pokemon/[id].tsx
@@ -0,0 +1,46 @@
+import { GetStaticProps, NextPage, GetStaticPaths } from 'next';
+
+import { pokeApi } from '../../api';
+import { Layout } from '../../components/layouts';
+import { Pokemon } from '../../interfaces';
+
+interface Props {
+    pokemon: Pokemon;
+}
+
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {   
+    
+    return (
+        <Layout title='Algún pokémon'>
+            <h1>{ pokemon.name }</h1>
+        </Layout>
+    )
+}
+
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+
+    const pokemons151 = [...Array(151)].map( ( value, index ) => `${ index + 1 }` );    
+
+    return {
+        paths: pokemons151.map( id => ({
+            params: { id }
+        })),
+        fallback: false
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+
+    const { id } = params as { id: string };
+
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ id }`);
+  
+    return {
+      props: {
+        pokemon: data
+      }
+    }
+  }
+
+export default PokemonPage;
