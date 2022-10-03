ee5ff94f3b39d9b6c5dd4d8bf53090bc233b3ce5
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-03 09:56:11 -0400

    Implementando Axios para consumir la API de los pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage, GetStaticProps } from 'next';
+import { pokeApi } from '../api';
 
 import { Layout } from '../components/layouts';
 
@@ -28,11 +29,11 @@ const HomePage: NextPage = ( props ) => {
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
 
-  console.log('Hola Mundo');
+  const { data } = await pokeApi.get('/pokemon?limit=151');
 
   return {
     props: {
-      name: 'Miguel'
+      pokemons: data.results
     }
   }
 }
