c639f4e1b3e24bb593060425cba9b3a128b63020
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-04 14:31:52 -0400

    Creando los componentes FavoriteCardPokemon y FavoritePokemons y enlazando cada item de favoritos a su página particular

--- components/pokemon/index.ts
+++ components/pokemon/index.ts
@@ -1,1 +1,3 @@
-export * from './PokemonCard'
+export * from './FavoriteCardPokemon';
+export * from './FavoritePokemons';
+export * from './PokemonCard';
--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
-import { Card, Grid } from '@nextui-org/react';
 
 import { Layout } from "../../components/layouts";
+import { FavoritePokemons } from '../../components/pokemon';
 import { NoFavorites } from "../../components/ui";
 import { localFavorites } from "../../utils";
 
@@ -19,24 +19,7 @@ const FavoritesPage = () => {
       {
         favoritePokemons.length === 0
           ? ( <NoFavorites /> )
-          : (
-
-            <Grid.Container gap={ 2 } direction='row' justify='flex-start'>
-              {
-                favoritePokemons.map( id => (
-                  <Grid xs={ 6 } sm={ 3 } md={ 2 } xl={ 1 } key={ id }>
-                    <Card hoverable clickable css={{ padding: 10 }}>
-                      <Card.Image 
-                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ id }.svg`}
-                        width={'100%'}
-                        height={ 140 }
-                      />
-                    </Card>                    
-                  </Grid>
-                ) )
-              }
-            </Grid.Container>
-          )
+          : ( <FavoritePokemons pokemons={ favoritePokemons } /> )
       }
         
 
