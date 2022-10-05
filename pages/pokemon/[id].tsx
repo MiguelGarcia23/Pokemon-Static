02b23f6909a478955766c52cec4ce1d7ad5e8265
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-05 15:01:47 -0400

    Implementando Incremental Static Regeneration (ISR) e Incremental Static Generation (ISG) en la búsqueda por name

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -115,7 +115,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         paths: pokemons151.map( id => ({
             params: { id }
         })),
-        fallback: false
+        fallback: 'blocking'
     }
 }
 
@@ -123,9 +123,21 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const { id } = params as { id: string };
 
+    const pokemon = await getPokemonInfo( id );
+
+    if ( !pokemon ) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
     return {
       props: {
-        pokemon: await getPokemonInfo( id )
+        pokemon,
+        revalidate: 86400,
       }
     }
 }
--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,13 +2,19 @@ import { pokeApi } from "../api";
 import { Pokemon } from "../interfaces";
 
 export const getPokemonInfo = async ( nameOrId: string ) => {
+    
+    try {
+        
+        const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ nameOrId }`);
+    
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites
+        }
 
-    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ nameOrId }`);
-
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites
+    } catch (error) {
+        return null;
     }
 
 }
